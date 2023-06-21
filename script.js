0a9f203944be40f95835b050d3e4508d5a7464dd
Safi Mosavi <safihaider0987@gmail.com>
2023-06-21 10:22:43 -0100

    version 1

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleList1(){
+    document.getElementById("id1").classList.toggle("show-description");
+}
+document.getElementById("id1").addEventListener("click", toggleList1)
+function toggleList2(){
+    document.getElementById("id2").classList.toggle("show-description");
+}
+document.getElementById("id2").addEventListener("click", toggleList2)
+function toggleList3(){
+    document.getElementById("id3").classList.toggle("show-description");
+}
+document.getElementById("id3").addEventListener("click", toggleList3)
